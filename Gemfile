b33310a8b047df4210cc0d5c194647df3618ddce
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-01-05 09:22:15 +0400

    attach bootstrap to project

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 gem 'hirb'
+gem 'bootstrap'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack");
+
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"] // for Bootstrap 4
+  })
+);
+
 
 module.exports = environment
