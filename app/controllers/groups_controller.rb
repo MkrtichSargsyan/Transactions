f366a815acad7f9da52847a4d471d7843d4bad5f
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-01-11 02:24:56 +0400

    create show groups transactions page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,14 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: %i[show edit update destroy]
 
   def index
     @groups = Group.all
   end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
