13c2d5c1fdc1c70aad06c2d1a48a1b69ffecd376
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-01-14 21:24:51 +0400

    add validation to add icon for creating new group

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,7 @@ class Group < ApplicationRecord
   has_one_attached :icon
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+  validates :icon, presence: true
 
   belongs_to :user
   has_many :transaction_groups, dependent: :destroy
